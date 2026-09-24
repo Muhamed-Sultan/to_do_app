d34a1f6014ae0247f6418d32f86310477219495d
Muhamed Salah Sultan <mohsala120@gmail.com>
2026-09-24 14:17:16 +0300

    ToDo APP

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ![alt text](image/s.png)
 ![alt text](image/f1.png)
 ![alt text](image/f.png)
-
+![alt text](image/s2.png)
+![alt text](image/s3.png)
--- lib/to_do_app.dart
+++ lib/to_do_app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:to_do_app/features/login/login_screen.dart';
 import 'package:to_do_app/features/splash/splash_screen.dart';
 
 class ToDoApp extends StatelessWidget {
